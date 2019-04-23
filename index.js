fa52346705540ef130adf7f2f32a2dd69d2a47dd
Farhan <farhanisto@gmail.com>
2019-04-23 17:20:12 +0300

    added the skeleton for the subscriptions

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const plansController = require('/controllers/plans-conroller')
+const subscriptionController = require('/controllers/subscription-controller')
 const port = process.env.port || 3000
 const app = express()
 
 app.use('api/plans', plansController)
+app.use('api/subscription', subscriptionController)
 app.listen(port => 
 console.log(`server is running on port ${port}`)
 );
